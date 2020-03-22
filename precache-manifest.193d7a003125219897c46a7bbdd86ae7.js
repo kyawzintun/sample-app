00aaef2b4dee526c0045388d263ef5056cde6e22
Kyaw Zin Tun <kyawzin.tun@yomabank.com>
2020-03-22 14:48:15 +0630

    Updates

---
+++ precache-manifest.193d7a003125219897c46a7bbdd86ae7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8834dbc2197de29b46a917d8bc1df64",
+    "url": "/sample-app/index.html"
+  },
+  {
+    "revision": "9ce15bbf3b84aaafccac",
+    "url": "/sample-app/static/css/main.48ae644a.chunk.css"
+  },
+  {
+    "revision": "7db8b316dd4eebe8f53a",
+    "url": "/sample-app/static/js/2.4351b205.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-app/static/js/2.4351b205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce15bbf3b84aaafccac",
+    "url": "/sample-app/static/js/main.23f81f61.chunk.js"
+  },
+  {
+    "revision": "62a58efaabb12746d365",
+    "url": "/sample-app/static/js/runtime-main.32b80380.js"
+  },
+  {
+    "revision": "38e17ea05da2b8044c5adc8164f566e6",
+    "url": "/sample-app/static/media/check-circle.38e17ea0.svg"
+  },
+  {
+    "revision": "8b8b8ce5483ce12e0fd638edf3060e85",
+    "url": "/sample-app/static/media/delete.8b8b8ce5.svg"
+  },
+  {
+    "revision": "e56214d1adbcfcd7c0e9a98719a98e78",
+    "url": "/sample-app/static/media/jump.e56214d1.jpg"
+  }
+]);
